059d8547806b6921234b05478f230b07adfa2e22
Gaurav Agarwal <82750742+gaurav-101@users.noreply.github.com>
2024-03-31 10:29:21 +0530

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const instance= axios.create({
 //     baseURL: 'http://localhost:8080'
 // })
-const BASE_URL = "http://localhost:8080/"
+const BASE_URL = "https://inventory-management-iiit.onrender.com/"
 // const BASE_URL = "https://inventory-backend-v1.herokuapp.com/"
 
 
